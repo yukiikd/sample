46bb205667564b257c67f21042907e3d14f2f259
chikashi <chikashi.ni.ym@gmail.com>
2015-04-05 17:55:56 +0900

    test file

--- person.rb
+++ person.rb
@@ -7,21 +7,18 @@ class Person
   sex = '男性'
 
 
-  put say_hello(name,kana,age,sex)
+  self.say_hello(name,kana,age,sex)
 
 
   def say_hello(name,kana,age,sex)
 
     if sex == '男性'
-      output = '私の名前は' +  name + 'です。ちょっと難しいですが、' + kana + 'と読みます。'
+      put '私の名前は' +  name + 'です。ちょっと難しいですが、' + kana + 'と読みます。'
        '今年で' + age + '歳になる、ちょっと暴れん坊です。'
     else
-      output =  '私の名前は' +  name + 'です。ちょっと難しいですが、' + kana + 'と読みます。'
+      put '私の名前は' +  name + 'です。ちょっと難しいですが、' + kana + 'と読みます。'
       '今年で' + age + '歳になる、ちょっとやんちゃな女の子です。'
     end
-    
-    return output
-    
   end
 
 
