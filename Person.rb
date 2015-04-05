ad44f742b9c6cd4400362b4702525212658f467c
ozeki <ozeki@MacBook-Pro.local>
2015-04-05 17:59:39 +0900

    person

---
+++ Person.rb
@@ -0,0 +1,23 @@
+class Person
+
+name="yuka"
+kana="yuka"
+age="22"
+sex=1
+
+def say_hello
+
+if sex = 0
+
+text = "私の名前は"+ name +"です。ちょっと難しいですが、"+kana+"と読みます。今年で"+age+"歳になる、ちょっと暴れん坊の男の子です。"
+
+else
+text = "私の名前は"+ name +"です。ちょっと難しいですが、"+kana+"と読みます。今年で"+age+"歳になる、ちょっとやんちゃな女の子です。"
+
+end
+
+text
+
+end
+
+end
